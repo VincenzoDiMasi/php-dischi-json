27a5d38475c4e84eba625ad1f6baf6db2924da06
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-09 16:41:24 +0100

    Add contents from API

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+
+$source_url = __DIR__ . '/data.json';
+
+$json_data = file_get_contents($source_url);
+
+$discs = json_decode($json_data, true);
+
+header('Content-type: application/json');
+
+echo json_encode($discs);
--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-console.log('VUE OK', Vue);
+// console.log('VUE OK', Vue);
+
+const apiUri = 'http://localhost:8888/php-dischi-json/api.php';
 
 const app = Vue.createApp({
-
+    data(){
+        return {
+    
+            discs: []
+        }
+    },
+    methods: {
+        getDiscs(){
+            axios.get(apiUri).then(res => {
+                this.discs = res.data;
+            })
+        }
+    },
+    mounted(){
+        this.getDiscs();
+    }
 });
 
 app.mount('#root');
